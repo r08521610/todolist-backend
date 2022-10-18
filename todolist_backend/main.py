4a4965c90c1f8ccf01903aec5749e792bb6bc221
Rainforest <rainforestnick@gmail.com>
2022-10-18 17:11:11 +0000

    feat: sqlite

--- todolist_backend/main.py
+++ todolist_backend/main.py
@@ -1,11 +1,14 @@
 from uuid import uuid4
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from todolist_backend.model import TodoItem
+from .database import engine, get_db
+from .models import Base, Todo
+from .schemas import TodoItem
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-todos = []
 
 
 @app.get("/")
@@ -14,18 +17,19 @@ async def root():
 
 
 @app.get("/todo")
-async def get_todos():
-  return todos
+async def get_todos(db: Session = Depends(get_db)):
+  return db.query(Todo).all()
 
 
 @app.get('/todo/{todo_id}')
-async def get_todo(todo_id: str):
-  print(todos, todo_id)
-  return next((item for item in todos if item['id'] == todo_id), None)
+async def get_todo(todo_id: str, db: Session = Depends(get_db)):
+  return db.query(Todo).filter(Todo.id == todo_id).first()
 
 
 @app.post('/todo')
-async def create_todo(todo: TodoItem):
-  todo = {'id': str(uuid4()), **todo.dict()}
-  todos.append(todo)
+async def create_todo(todo: TodoItem, db: Session = Depends(get_db)):
+  todo = Todo(**todo.dict())
+  db.add(todo)
+  db.commit()
+  db.refresh(todo)
   return todo
